f69d8059abf9c9292c6ae2028947aed9d9bdb575
Mir Tanim Ahmed <mtahmed_dev@proton.me>
2025-01-28 12:08:21 +0600

    user

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -17,47 +17,58 @@ const get = async (req: Request, res: Response, next: NextFunction): Promise<any
     email: email,
     contact: user.phoneNumber,
     address: user.address,
-    avatar: user.avatar,
+    avatar: user.avatar
   };
   return res.status(StatusCodes.OK).json({ success: true, message: "User data retrieved successfully.", data: data });
 };
 
 const update = async (req: Request, res: Response, next: NextFunction): Promise<any> => {
-  const { name, contact, address, avatarUrl } = req.body;
-  const userId = req.user.userId;
-  const email = req.user.email;
-  let error, user;
+  const userId = req.params.id;
+  const updates = req.body;
 
-  if (!name && !contact && !address && !avatarUrl) {
-    return next(createError(StatusCodes.BAD_REQUEST, "At least one field should be updated."));
-  }
-
-  [error, user] = await to(User.findById(userId));
+  const [error, user] = await to(User.findByIdAndUpdate(userId, { $set: updates }, { new: true }));
   if (error) return next(error);
   if (!user) return next(createError(StatusCodes.NOT_FOUND, "User not found."));
 
-  user.name = name || user.name;
-  user.phoneNumber = contact || user.phoneNumber;
-  user.address = address || user.address;
-  user.avatar = avatarUrl || user.avatar;
-
-  [error] = await to(user.save());
-  if (error) return next(error);
-
-  const data = {
-    name: user.name,
-    email: email,
-    contact: user.phoneNumber,
-    address: user.address,
-    avatar: user.avatar,
-  };
-
-  return res.status(StatusCodes.OK).json({ success: true, message: "User updated successfully.", data: data });
+  return res.status(StatusCodes.OK).json({ success: true, message: "Success", data: user });
 };
+
+// const update = async (req: Request, res: Response, next: NextFunction): Promise<any> => {
+//   const { name, contact, address, avatarUrl } = req.body;
+//   const userId = req.user.userId;
+//   const email = req.user.email;
+//   let error, user;
+//
+//   if (!name && !contact && !address && !avatarUrl) {
+//     return next(createError(StatusCodes.BAD_REQUEST, "At least one field should be updated."));
+//   }
+//
+//   [error, user] = await to(User.findById(userId));
+//   if (error) return next(error);
+//   if (!user) return next(createError(StatusCodes.NOT_FOUND, "User not found."));
+//
+//   user.name = name || user.name;
+//   user.phoneNumber = contact || user.phoneNumber;
+//   user.address = address || user.address;
+//   user.avatar = avatarUrl || user.avatar;
+//
+//   [error] = await to(user.save());
+//   if (error) return next(error);
+//
+//   const data = {
+//     name: user.name,
+//     email: email,
+//     contact: user.phoneNumber,
+//     address: user.address,
+//     avatar: user.avatar,
+//   };
+//
+//   return res.status(StatusCodes.OK).json({ success: true, message: "User updated successfully.", data: data });
+// };
 
 const UserController = {
   get,
-  update,
+  update
 };
 
 export default UserController;
