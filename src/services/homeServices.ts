08c95e75d0323162362af524c20f7c321cd8811f
Mir Tanim Ahmed <mtahmed_dev@proton.me>
2025-02-05 12:27:45 +0600

    change in home

--- src/services/homeServices.ts
+++ src/services/homeServices.ts
@@ -4,10 +4,14 @@ import to from "await-to-ts";
 import createError from "http-errors";
 import { StatusCodes } from "http-status-codes";
 import SubscriptionPlan from "@models/subscriptionPlanModel";
+import User from "@models/userModel";
 
 const homeData = async (req: Request, res: Response, next: NextFunction): Promise<any> => {
   const userId = req.user.userId;
-  let error, podcast, subscriptionPlans;
+  let error, user, podcast, subscriptionPlans;
+
+  [error, user] = await to(User.findById(userId).populate({path: "auth", select: "email"}));
+  if(error) return next(error);
 
   [error, podcast] = await to(
     Podcast.findOne({ primaryUser: userId })
@@ -30,7 +34,7 @@ const homeData = async (req: Request, res: Response, next: NextFunction): Promis
   [error, subscriptionPlans] = await to(SubscriptionPlan.find().lean());
   if (error) return next(error);
 
-  return res.status(StatusCodes.OK).json({ success: true, message: "Success", data: { podcast, subscriptionPlans } });
+  return res.status(StatusCodes.OK).json({ success: true, message: "Success", data: { user, podcast, subscriptions } });
 };
 
 const HomeServices = {
